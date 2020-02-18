ace77aad015982c716265a2b62d084fa9bdc864f
Michael Walker <mike@barrucadu.co.uk>
2020-02-18 01:34:34 +0000

    Add a script to build & push the initial docker images

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+cat ~/secrets/nixos/dreamlands/registry-password.txt | docker login --username registry --password-stdin https://registry.barrucadu.dev
+
+cd event-api-resource
+docker build -t registry.barrucadu.dev/event-api-resource:latest .
+docker push registry.barrucadu.dev/event-api-resource:latest
+
+cd ../event-api-server
+docker build -t registry.barrucadu.dev/event-api-server:latest .
+docker push registry.barrucadu.dev/event-api-server:latest
+
+cd ../frontend
+docker build -t registry.barrucadu.dev/frontend:latest .
+docker push registry.barrucadu.dev/frontend:latest
