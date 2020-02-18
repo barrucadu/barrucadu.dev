9ec87089d31ffbda7617db94677079c9f8197bc2
Michael Walker <mike@barrucadu.co.uk>
2020-02-18 18:56:57 +0000

    Import rsync-resource from concoursefiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,3 +15,7 @@ docker push registry.barrucadu.dev/event-api-server:latest
 cd ../frontend
 docker build -t registry.barrucadu.dev/frontend:latest .
 docker push registry.barrucadu.dev/frontend:latest
+
+cd ../rsync-resource
+docker build -t registry.barrucadu.dev/rsync-resource:latest .
+docker push registry.barrucadu.dev/rsync-resource:latest
